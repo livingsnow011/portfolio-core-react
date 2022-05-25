c23fc29d4fbfa6749947bcd56a0125965f079eaa
SOL <livingsnow011@gmail.com>
2022-05-25 20:24:49 +0900

    contact react emailjs 완료

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import  React,{useState,useEffect,useRef} from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faSearch } from "@fortawesome/free-solid-svg-icons";
+import emailjs from '@emailjs/browser';
 
 function App() {
   return (
@@ -258,6 +259,30 @@ function Projects(){
 }
 
 function Contact(){
+
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+    let templateParams  = {
+        name : document.getElementById('userName').value,
+        phone : document.getElementById('phone').value,
+        email : document.getElementById('userEmail').value,
+        message : document.getElementById('message').value,
+      }
+    if(!templateParams.name) return alert("성함을 입력해주세요");
+    if(!templateParams.email) return alert("메일을 입력해주세요");
+    if(!templateParams.message) return alert("메일 본문을 입력해주세요");
+
+    emailjs.sendForm('service_3suz5b8', 'template_lctllas', form.current, 'DjTFzTKBO3anEGS1j')
+      .then((result) => {
+          console.log('Success!', result.status, result.text);
+          alert("메일을 성공적으로 보냈습니다.")
+      }, (error) => {
+        console.log('Failed...', error);
+        alert("메일을 전송하는데 실패했습니다.\n"+"livingsnow@gmail.com 으로 메일을 보내주세요");
+      });
+  };
+
   return (
     <section id="contact" className="section">
       <div className="section-title">
@@ -269,12 +294,12 @@ function Contact(){
       </div>
 
       <div className="section__wrapper contact__wrapper">
-        <form id="contact-form" className="contact__form">
-          <input type="text" className="input" id="userName" placeholder=" 성함 (필수)" />             
-          <input type="text" className="input" id="phone" placeholder=" 연락처 (생략 가능)" /><br/>
-          <input type="text" className="input" id="userEmail" placeholder=" 보내주시는 분의 메일 주소를 입력해주세요 (필수)" /><br/>
-          <textarea id="message" className="input" rows="5" placeholder=" 내용을 입력해주세요 (필수)"></textarea><br/>
-          <input type="button" id="btn" className="contact__form-sendBtn" value="Send" onClick="sendEmail()"/>
+        <form ref={form} onSubmit={sendEmail} id="contact-form" className="contact__form">
+          <input type="text" className="input" name="user_name" id="userName" placeholder=" 성함 (필수)" />             
+          <input type="text" className="input"  id="phone" placeholder=" 연락처 (생략 가능)" /><br/>
+          <input type="text" className="input"name="user_email"  id="userEmail" placeholder=" 보내주시는 분의 메일 주소를 입력해주세요 (필수)" /><br/>
+          <textarea id="message" name="message" className="input" rows="5" placeholder=" 내용을 입력해주세요 (필수)"></textarea><br/>
+          <input type="submit" id="btn" className="contact__form-sendBtn" value="Send"/>
         </form>
       </div>
     </section>
@@ -284,7 +309,7 @@ function Contact(){
 
 function Footer(){
   return(
-    <footer >
+    <footer className="footer">
       <p>
       </p>
     </footer>
