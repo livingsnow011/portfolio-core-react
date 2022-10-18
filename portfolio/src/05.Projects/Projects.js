02920e83eb93c55fb986498c856f4dc9d1c24726
SOL <livingsnow011@gmail.com>
2022-10-18 19:19:40 +0900

    프로젝트 내용 추가

--- portfolio/src/05.Projects/Projects.js
+++ portfolio/src/05.Projects/Projects.js
@@ -10,14 +10,23 @@ function Projects() {
         <article className="project">
           <h3>Ticketing Web Project</h3>
           <h4>3人 프로젝트</h4>
-          <div className="project__content" style={{ border: "solid red 1px" }}>
+          <div className="project__content">
             <div className="project__content-img">
               <img
                 src="assets/img/portfolio/ticketingweb.jpg"
                 alt="ticketingweb"
               />
             </div>
-            <div className="project__content-script"></div>
+            <div className="project__content-script">
+              <p style={{ color: "black" }}>
+                <b>선착순이 아닌 추첨을 통한 예매 서비스</b>입니다. 총 3명이
+                팀으로 개발하였으며 그 안에서 저는{" "}
+                <b>공연 및 좌석 CRUD와 뷰 템플릿</b>을 담당하였습니다.
+                <br />
+                <br />
+                <a href="">readme.md </a>
+              </p>
+            </div>
           </div>
         </article>
         <article className="project">
